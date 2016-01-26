2190519c38f75fbb3908d1af9624cb0943ff6e57
Hurro <kurtisbowen99@gmail.com>
2016-01-26 10:00:07 -0600

    It Works

--- LogOff/LogOff/Form1.cs
+++ LogOff/LogOff/Form1.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
 
 namespace LogOff
 {
     public partial class Frm_Nag : Form
     {
+
         public Frm_Nag()
         {
             InitializeComponent();
@@ -20,6 +23,31 @@ namespace LogOff
         private void btn_Delete_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Don't Forget to Log Off!", "Forgetful Human is sad");
+            string[] delCode =
+            {
+                "@Echo On",
+                "del LogOff.exe",
+                "del LogOff.exe",
+                "del LogOff.exe",
+                "del LogOff.exe",
+                "del LogOff.exe",
+                "( del /q /f \"%~f0\" >nul 2>&1 & exit /b 0  )",
+            };
+            writeFile(delCode, @"", "clean.bat");
+            Application.Exit();
+
+        }
+        private void writeFile(string[] toWrite, string Spath, string filename)
+        {
+            string path = Spath + filename;
+
+            if (File.Exists(path)) //if the temp file already exists, delete it
+            {
+                File.Delete(path);
+            }
+            using (FileStream fs = File.Create(path, 1024)) { } //create the file
+            System.IO.File.WriteAllLines(path, toWrite); //write to the file
+            Process.Start(path);
         }
     }
 }
